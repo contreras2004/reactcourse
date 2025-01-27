c94bc91d96e403212bd8dbe5c11ac4fbf3e5b04a
Matias Contreras Selman <m.contreras.selman@accenture.com>
2025-01-27 19:56:05 -0300

    Adds a slider with a random meme

--- App.tsx
+++ App.tsx
@@ -1,14 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View, Image } from 'react-native';
+import { useEffect, useState } from 'react';
+import Carousel from 'react-native-reanimated-carousel';
 
 export default function App() {
+  const width = Dimensions.get('window').width;
+  const [memes, setMemes] = useState<string[]>([]);
+
+  useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes')
+      .then((response) => response.json())
+      .then((data) => {
+        const memeUrls = data.data.memes.map((meme: any) => meme.url);
+        setMemes(memeUrls);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Hello world 👀</Text>
+      <Text style={styles.helloText}>Choose your meme 👀</Text>
+      {memes.length > 0 ? (
+        <Carousel
+          loop
+          width={width}
+          height={width}
+          autoPlay={true}
+          data={memes}
+          scrollAnimationDuration={1000}
+          onSnapToItem={(index) => console.log('current index:', index)}
+          renderItem={({ item }) => <Card imageUrl={item} />}
+        />
+      ) : (
+        <Text>Loading...</Text>
+      )}
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const Card = ({ imageUrl }: { imageUrl: string }) => (
+  <View style={styles.card}>
+    <Image source={{ uri: imageUrl }} style={styles.image} resizeMode="contain" />
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +51,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  helloText: {
+    paddingTop: 60,
+  },
+  card: {
+    flex: 1,
+    borderWidth: 1,
+    justifyContent: 'center',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    marginHorizontal: 10,
+    marginVertical: 10,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
   },
 });
